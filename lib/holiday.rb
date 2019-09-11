e7246fbd097ae08efecafab520ff0d411c425c2e
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-11 20:07:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
   holiday_hash.each do |season, holidays|
-    binding.pry
+    holidays.each do |holiday, supplies|
+      binding.pry
+    end
   end
 
 
